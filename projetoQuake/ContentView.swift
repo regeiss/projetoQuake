9d058cfcdc4d268f2a49ef1a4d32b8420c245e8e
Roberto Edgar Geiss <roberto.geiss.nh@gmail.com>
2022-06-21 20:49:22 -0300

    Initial Commit

---
+++ projetoQuake/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  projetoQuake
+//
+//  Created by Roberto Edgar Geiss on 21/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ projetoQuake/projetoQuakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  projetoQuakeApp.swift
+//  projetoQuake
+//
+//  Created by Roberto Edgar Geiss on 21/06/22.
+//
+
+import SwiftUI
+
+@main
+struct projetoQuakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
