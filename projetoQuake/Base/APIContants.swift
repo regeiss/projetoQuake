c6707460569c2e808136a0ab7df96605640a8fbf
Roberto Edgar Geiss <15005694+regeiss@users.noreply.github.com>
2022-06-22 17:16:15 -0300

    Update APIContants.swift

--- projetoQuake/Base/APIContants.swift
+++ projetoQuake/Base/APIContants.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+
+protocol APIBuilder {
+    var urlRequest: URLRequest { get }
+    var baseUrl: URL { get }
+    var path: String { get }
+}
+
+enum QuakeAPI {
+    case getNews
+}
+
+extension QuakeAPI: APIBuilder {
+
+    var baseUrl: URL {
+        switch self {
+        case .getNews:
+            return URL(string: "https://earthquake.usgs.gov/fdsnws/event/1/")!
+        }
+    }
+    
+    var path: String {
+        switch self {
+        case .getNews:
+            return "/news"
+
+        }
+    }
+    
+    var urlRequest: URLRequest {
+        switch self {
+        case .getNews:           
+            return URLRequest(url: self.baseUrl.appendingPathComponent(self.path))
+        }
+    }
+}
