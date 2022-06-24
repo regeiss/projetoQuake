d28b10dce6d7228267e0e900d84c3848de34d7ca
Roberto Edgar Geiss <15005694+regeiss@users.noreply.github.com>
2022-06-24 11:27:22 -0300

    Update QuakeViewModel.swift
    Implementada viewmodel

--- projetoQuake/Feature/Quake/ViewModel/QuakeViewModel.swift
+++ projetoQuake/Feature/Quake/ViewModel/QuakeViewModel.swift
@@ -6,3 +6,47 @@
 //
 
 import Foundation
+
+protocol QuakesViewModel: ObservableObject
+{
+    func getAllQuakes() async
+}
+
+@MainActor
+final class QuakesViewModelImpl: QuakesViewModel
+{
+    enum State 
+    {
+        case na
+        case loading
+        case success(data: [Quake])
+        case failed(error: Error)
+    }
+
+    @Published private(set) var Quakes: [Quake] = []
+    @Published private(set) var state: State = .na
+    @Published var hasError: Bool = false
+    
+    private let service: QuakesService
+
+    init(service: QuakesService)
+    {
+        self.service = service
+    }
+    
+    func getAllQuakes() async
+    {
+        self.state = .loading
+        self.hasError = false
+        do
+        {
+            let data = try await service.fetch()
+            self.state = .success(data: data)
+        }
+        catch
+        {
+            self.state = .failed(error: error)
+            self.hasError = true
+        }
+    }
+}
