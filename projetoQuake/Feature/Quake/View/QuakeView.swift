90aa8ea9175b892590c93265c61607e18b9746c2
Roberto Edgar Geiss <15005694+regeiss@users.noreply.github.com>
2022-06-24 13:42:00 -0300

    Update QuakeView.swift
    Implementação inicial

--- projetoQuake/Feature/Quake/View/QuakeView.swift
+++ projetoQuake/Feature/Quake/View/QuakeView.swift
@@ -4,17 +4,45 @@
 //
 //  Created by Roberto Edgar Geiss on 23/06/22.
 //
-
 import SwiftUI
 
-struct QuakeView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct QuakeView: View
+{
+    let item: Quake
+     
+    var body: some View
+    {
+        VStack(alignment: .leading, spacing: 8)
+        {
+             Text(makeAttributedString(title:"userID", label:String(item.userId)))
+             Text(makeAttributedString(title:"Titulo", label: item.title))
+             Text(makeAttributedString(title:"Texto", label: item.body))
+         }
+         .padding()
+         .foregroundColor(.black)
+    }
+    
+    private func makeAttributedString(title: String, label: String) -> AttributedString
+    {
+        var string = AttributedString("\(title): \(label)")
+        string.foregroundColor = .label
+        string.font = .system(size: 16, weight: .bold)
+        
+        if let range = string.range(of: label)
+        {
+            string[range].foregroundColor = .systemGray
+            string[range].font = .system(size: 16, weight: .regular)
+        }
+        return string
     }
 }
 
-struct QuakeView_Previews: PreviewProvider {
-    static var previews: some View {
-        QuakeView()
+struct QuakeView_Previews: PreviewProvider
+{
+    static var previews: some View
+    {
+        QuakeView(item: Quake.dummyData.first!)
+            .preview(displayName: "Quake View")
     }
 }
+
