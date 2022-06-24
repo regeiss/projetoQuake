406ff36ff029c07845db648e7d7578eacf72d6c8
Roberto Edgar Geiss <15005694+regeiss@users.noreply.github.com>
2022-06-24 11:10:43 -0300

    Update QuakeService.swift
    Serviço de get implementado

--- projetoQuake/Feature/Quake/Service/QuakeService.swift
+++ projetoQuake/Feature/Quake/Service/QuakeService.swift
@@ -7,5 +7,25 @@
 
 import Foundation
 
-protocol QuakeServices
-{}
+enum QuakesServiceError: Error 
+{
+    case failed
+    case failedToDecode
+    case invalidServerResponse
+}
+
+protocol QuakesService
+{
+    func fetch() async throws -> [Quake]
+}
+
+final class QuakesServiceImpl: QuakesService
+{
+    func fetch() async throws -> [Quake]
+        {
+            let urlSession = URLSession.shared
+            let url = URL(string: APIConstants.baseURL.appending("/posts")) // ajustar
+            let (data, _) = try await urlSession.data(from: url!)
+            return try JSONDecoder().decode([Quake].self, from: data)
+        }
+}
